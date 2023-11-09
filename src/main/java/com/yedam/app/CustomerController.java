6051f11cad2fd3743bff0b252c1d8ef183b0971d
jang <rldjq0308@naver.com>
2023-11-09 10:37:59 +0900

    controller 수정

--- src/main/java/com/yedam/app/CustomerController.java
+++ src/main/java/com/yedam/app/CustomerController.java
@@ -3,6 +3,7 @@ package com.yedam.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.yedam.app.accessingdatajpa.Customer;
 import com.yedam.app.accessingdatajpa.CustomerRepository;
@@ -10,6 +11,13 @@ import com.yedam.app.accessingdatajpa.CustomerRepository;
 @RestController // Controller, ResponseBody
 public class CustomerController {
 	@Autowired CustomerRepository repo;
+	
+	@GetMapping("/")
+	public ModelAndView main() {
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("greet","<b>안녕</b>하세요");
+		return mv;
+	}
 	
 	@GetMapping("/customer")
 	public Iterable<Customer> findall(){
